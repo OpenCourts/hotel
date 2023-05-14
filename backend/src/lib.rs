4b4d48bd17c61fc079e51480dfac1944d8b4937c
Paul Koehler <paulkoehler@hotmail.com>
2023-05-15 00:53:59 +0200

    add commented out 404 catcher

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -1,12 +1,18 @@
 #[macro_use]
 extern crate rocket;
 
-use rocket::{Build, Rocket};
+use rocket::{Build, Request, Rocket};
 use rocket::fs::FileServer;
 
 pub mod api;
 pub mod model;
 mod sqlx;
+
+
+/*#[catch(404)]
+fn not_found(req: &Request) -> String {
+    format!("Sorry, '{}' is not a valid path.", req.uri())
+}*/
 
 #[launch]
 pub fn rocket() -> Rocket<Build> {
@@ -14,6 +20,7 @@ pub fn rocket() -> Rocket<Build> {
     let mut rc = rocket::build().manage(app_data)
         .attach(sqlx::stage())
         .mount("/", FileServer::from("static/"));
+        //.register("/", catchers![not_found]);
     // mount the api (/api)
     rc = api::build_api(rc, "/api");
     rc
